69a14fbed1daafd7c54ddf7f1b64a30410256090
flanny7 <mottei.senshu@gmail.com>
2018-09-04 05:32:32 +0900

    Add Utility Scripts And CommonScene

---
+++ Assets/20_Scripts/Base/SingletonMonoBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+{
+    private bool isInit;
+
+    protected SingletonMonoBehaviour()
+    {
+        this.isInit = false;
+    }
+
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            instance = FindObjectOfType(typeof(T)) as T;
+
+            if (instance == null)
+            {
+                Debug.LogError("<color=#f00>" + typeof(T) + "</color> is nothing");
+                Debug.Break();
+            }
+
+            if (instance != null && !instance.isInit)
+            {
+                instance.Init();
+            }
+
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// 非明示的なoverrideをしないで!!
+    /// </summary>
+    private void Awake()
+    {
+        this.Init();
+    }
+
+    /// <summary>
+    /// 初期化関数
+    /// </summary>
+    private void Init()
+    {
+        if (this == Instance)
+        {
+            if (this.isInit) { return; }
+
+            this.OnAwake();
+            this.isInit = true;
+
+            return;
+        }
+
+        Debug.LogError("<color=#f00>" + typeof(T) + "</color> is duplicated");
+        Debug.Break();
+    }
+    
+    protected abstract void OnAwake();
+}
---
+++ Assets/20_Scripts/Utility/RiaSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RiaSceneManager : SingletonMonoBehaviour<RiaSceneManager>
+{
+    protected override void OnAwake()
+    {
+        StartCoroutine(this.CheckManagers());
+    }
+
+    private IEnumerator CheckManagers()
+    {
+        while (true)
+        {
+            if (FadeManager.Instance) { break; }
+            if (AudioManager.Instance) { break; }
+
+            yield return null;
+        }
+    }
+}
