8894026db835ec2ffe301ffe1517988efd592326
flanny7 <mottei.senshu@gmail.com>
2018-09-09 02:24:14 +0900

    KettyEnemyの移植とリファクタリング

---
+++ Assets/20_Scripts/20_Game/Enemy/StraightEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public sealed class StraightEnemy : Enemy
+{
+    [SuppressMessage("ReSharper", "StyleCop.SA1401")]
+    [System.Serializable]
+    public class StraightEnemyDebugParam
+    {
+    }
+
+    // protected override void Awake()
+    // {
+    //     base.Awake();
+    // }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void Update()
+    {
+        base.Update();
+        // this.ElapsedTime += UnityEngine.Time.deltaTime;
+
+        // _
+        StraightMove();
+    }
+
+    // protected override void OnDisable()
+    // {
+    //     base.OnDisable();
+    // }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void StraightMove()
+    {
+        var pos = this.Trans.position;
+        pos.y += -this.MoveSpeedRate * Time.deltaTime;
+        this.Trans.position = pos;
+    }
+
+}
