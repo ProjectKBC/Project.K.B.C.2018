74c1b11dcb77e030643f2e4910da9fde09a43b0f
cross <ne280107@senshu-u.jp>
2018-09-17 05:40:42 +0900

    アイロスの通常ショット
    をとりあえず実装した(実はジェネラルショットをコピペしただけなのはナイショ)

---
+++ Assets/20_Scripts/20_Game/Player/Bullet/AilosNB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AilosNB : MonoBehaviour {
+    [SerializeField]
+    private float shotSpeed = 60;
+    private Vector3 shotStartPos;
+    [SerializeField]
+    private float shotRange = 70;
+
+    private void Start()
+    {
+        shotStartPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        this.transform.position += Vector3.up * shotSpeed * Time.deltaTime;
+
+        if (shotRange < Mathf.Abs(shotStartPos.y - this.transform.position.y))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/20_Scripts/20_Game/Player/Player/AilosPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AilosPlayer : PlayerMove {
+    [SerializeField]
+    private GameObject normalBulletPrefab = null;
+    [SerializeField]
+    private int shotInterval = 3;
+
+    // 弾を撃った後の経過時間
+    private int shotTimeCount;
+
+    private void Update()
+    {
+        base.Move();
+        NormalShot();
+    }
+
+    private void NormalShot()
+    {
+        switch (this.tag)
+        {
+            case "Player1":
+                // z キーを押している間
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    shotTimeCount++;
+                    if (shotInterval < shotTimeCount)
+                    {
+                        shotTimeCount = 0;
+
+                        GameObject normalBullets = Instantiate(normalBulletPrefab);
+                        normalBullets.transform.position = this.transform.position;
+                    }
+                }
+
+                break;
+
+            case "Player2":
+                // m キーを押している間
+                if (Input.GetKey(KeyCode.M))
+                {
+                    shotTimeCount++;
+                    if (shotInterval < shotTimeCount)
+                    {
+                        shotTimeCount = 0;
+
+                        GameObject normalBullets = Instantiate(normalBulletPrefab);
+                        normalBullets.transform.position = this.transform.position;
+                    }
+                }
+
+                break;
+        }
+    }
+}
