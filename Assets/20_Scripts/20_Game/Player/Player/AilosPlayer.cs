ace24ea60e69dd5625a4e3e0d4375485eec0fd06
cross <ne280107@senshu-u.jp>
2018-10-03 19:36:55 +0900

    よくわかんねぇなぁ
    あとちょっとでアイロスの特殊ショットなんとかなると思うんだけどなぁ

--- Assets/20_Scripts/20_Game/Player/Player/AilosPlayer.cs
+++ Assets/20_Scripts/20_Game/Player/Player/AilosPlayer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 /// <summary>
 /// ショットのボタンは仮の設定です
 /// </summary>
@@ -10,6 +11,13 @@ public class AilosPlayer : PlayerMove {
     private int shotInterval = 3;
     [SerializeField]
     private float spShotRange = 60.0f;
+    [SerializeField]
+    private float searchTime = 3.5f;
+    [SerializeField]
+    private int serchNumOfTimes = 3;
+
+    private GameObject[] targetEnemys = new GameObject[3];
+
 
     // 弾を撃った後の経過時間
     private int shotTimeCount;
@@ -18,6 +26,7 @@ public class AilosPlayer : PlayerMove {
     {
         base.Move();
         NormalShot();
+        SpecialShot();
     }
 
     private void NormalShot()
@@ -49,10 +58,18 @@ public class AilosPlayer : PlayerMove {
         switch (this.tag)
         {
             case "Player1":
-                // x キーを押している間
-                if (Input.GetKey(KeyCode.X))
+                // x キーを押した瞬間
+                if (Input.GetKeyDown(KeyCode.X))
                 {
-                    
+                    StartCoroutine( LockOn(serchNumOfTimes) );
+                }
+                if (Input.GetKeyUp(KeyCode.X))
+                {
+                    for (int i = 0; i < targetEnemys.Length; i++)
+                    {
+                        Destroy(targetEnemys[i]);
+                        targetEnemys[i] = null;
+                    }
                 }
                 break;
 
@@ -71,5 +88,44 @@ public class AilosPlayer : PlayerMove {
             GameObject normalBullets = Instantiate(normalBulletPrefab);
             normalBullets.transform.position = this.transform.position;
         }
+    }
+
+    private IEnumerator LockOn(int _serchNumOfTimes)
+    {
+        if(Input.GetKey(KeyCode.X))
+        {
+            for (int i = 0; i < _serchNumOfTimes; i++)
+            {
+                yield return new WaitForSeconds(searchTime);
+                if (NearSearchEnemy(spShotRange) != null)
+                {
+                    targetEnemys[i] = NearSearchEnemy(spShotRange);
+                    Debug.Log(targetEnemys[i]);
+                }
+            }
+        }
+
+    }
+
+    private GameObject NearSearchEnemy(float _searchRadius)
+    {
+        GameObject targetEnemy = null;
+        float tmpDistance = 0;
+        float nearDistance = 0;
+
+        foreach (GameObject obs in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            if(0 <= System.Array.IndexOf(targetEnemys, obs)) { break; }
+            tmpDistance = Vector3.Distance(obs.transform.position, this.transform.position);
+            if (tmpDistance <= _searchRadius)
+            {
+                if (nearDistance == 0 || tmpDistance < nearDistance)
+                {
+                    nearDistance = tmpDistance;
+                    targetEnemy = obs;
+                }
+            }
+        }
+        return targetEnemy;
     }
 }
