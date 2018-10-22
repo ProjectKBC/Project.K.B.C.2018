fe1fe24deee05b0cdd56b0b3cef002b8065ed504
cross <ne280107@senshu-u.jp>
2018-10-22 11:44:07 +0900

    アノマの通常ショット
    これアイロスくんの上位互換やんな？？？

---
+++ Assets/20_Scripts/20_Game/Player/Player/AnomaPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnomaPlayer : PlayerData {
+	[SerializeField]
+	private GameObject normalBulletPrefab = null;
+	[SerializeField]
+	private int normalShotInterval = 2;
+
+	private int normalShotTimeCount;
+
+	private void Update()
+	{
+		base.Move();
+		NormalShot();
+	}
+
+	private void NormalShot()
+	{
+		if (Input.GetKey(NormalShotKey))
+		{
+			CreateBullet();
+		}
+	}
+
+	private void CreateBullet()
+	{
+		normalShotTimeCount++;
+		if (normalShotInterval < normalShotTimeCount)
+		{
+			normalShotTimeCount = 0;
+
+			GameObject normalBullets = Instantiate(normalBulletPrefab);
+			normalBullets.transform.position = this.transform.position;
+		}
+	}
+
+}
